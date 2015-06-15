55b15a46e92c92a8e95b470d87bf2ada35b23819
huasheng <337075@qq.com>
2015-06-16 07:38:42 +0800

    hidden button

--- AlipaySubmit.php
+++ AlipaySubmit.php
@@ -104,7 +104,7 @@ class AlipaySubmit {
         }
 
         //submit按钮控件请不要含有name属性
-        $sHtml = $sHtml."<input type='submit' value='".$button_name."'></form>";
+        $sHtml = $sHtml."<input type='submit' value='".$button_name."' style='display:none;'></form>";
 
         $sHtml = $sHtml."<script>document.forms['alipaysubmit'].submit();</script>";
 
